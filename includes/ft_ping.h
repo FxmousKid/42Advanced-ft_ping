8a817a0b93a863cada43db52d66d42a5d6fc64e5
Iyan Nazarian <36068362+FxmousKid@users.noreply.github.com>
2025-07-24 18:03:41 +0300

    feat: adding basic headers

---
+++ includes/ft_ping.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ping.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: inazaria <inazaria@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/24 13:55:48 by inazaria          #+#    #+#             */
+/*   Updated: 2025/07/24 14:28:44 by inazaria         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PING_H
+# define FT_PING_H
+
+# include <stdio.h>
+# include <stdlib.h>
+
+# include "ANSI-colors.h"
+
+# define FUNC_SUCCESS 1
+# define FUNC_FAILURE 0
+
+# define BAD_ARG		RED "[FATAL] Bad Argument count\n" WHT
+# define INVALID_ARG	RED "[FATAL] Invalid arguments\n" WHT
+
+#endif
