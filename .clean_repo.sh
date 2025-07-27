4189862cc237e04929a1f375462075810eb804ca
Iyan Nazarian <36068362+FxmousKid@users.noreply.github.com>
2025-07-27 19:09:08 +0300

    feat: adding clean_repo script to run when submitting to vogsphere

---
+++ .clean_repo.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+txtgrn='\e[0;32m' # Green
+txtylw='\e[0;33m' # Cyan
+txtwht='\e[0;37m' # White
+
+# To use when submitting to the official vogsphere repo
+
+rm -rf ./.git ./en.subject.pdf ./ ./README.md ./.gitignore ./compile_commands.json ./build ./ft_ping
+echo -e "${txtgrn}Successfully Cleaned the repo for submitting !${txtwht}"
+echo -e "${txtylw}Now deleting the script...!${txtwht}"
+
+rm $(basename "$0")
