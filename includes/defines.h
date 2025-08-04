72d6ebae71c4d10d308e6b2d12997c0e2abf4019
Iyan Nazarian <36068362+FxmousKid@users.noreply.github.com>
2025-08-04 18:31:11 +0300

    chore: restructured headers into more subtle defines.h and ft_ping.h

---
+++ includes/defines.h
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   defines.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: inazaria <inazaria@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/04 16:57:10 by inazaria          #+#    #+#             */
+/*   Updated: 2025/08/04 17:56:03 by inazaria         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef DEFINES_H
+
+#include <stdio.h> // IWYU pragma: keep
+#include "ANSI-colors.h"
+
+# ifndef LIBFT_PATH
+#  define LIBFT_PATH "../libft/includes/libft.h"
+# endif
+
+// Errors 
+# define EXIT_SUCCESS	0
+# define EXIT_NO_RPL	1
+# define EXIT_OTHER	2
+# define EXIT_BAD_ARGS	64
+
+# define FUNC_SUCCESS 1
+# define FUNC_FAILURE 0
+
+# define ARG_MISSING(name)		fprintf(stderr, "%s: missing host operator.\n", (name))
+# define OPT_SHORT_INV(name,opt) 	fprintf(stderr, "%s: invalid option -- '%s'\n", (name), (opt))
+# define OPT_LONG_INV(name,opt)		fprintf(stderr, "%s: unrecognized option -- '%s'\n", (name), (opt))
+# define MORE_INFO_MSG(name)		fprintf(stderr, "Try '%s --help' for more information.\n", (name))
+
+# define LOGFILE_NAME	"logfile.txt"
+// # define LOGFILE_NAME	""
+
+	
+
+# define VERSION "\
+ft_ping 1.0\n\
+No Copyright (C) 2025 inazaria.\n\
+This is free software: you are free to change and redistribute it.\n\
+There is NO WARRANTY, to the extent permitted by law.\n\n\
+Written by Iyan Nazarian.\n\
+"
+
+// Networking
+
+# define ICMP_TYPE_ECHO_REQUEST	8
+# define ICMP_TYPE_ECHO_REPLY	0
+
+# define ICMP_CODE_ECHO_REQUEST 0
+# define ICMP_CODE_ECHO_REPLY 0
+
+# define IPV4_REGEX "[:number:]{3}"
+
+#endif
--- includes/ft_ping.h
+++ includes/ft_ping.h
@@ -6,7 +6,7 @@
 /*   By: inazaria <inazaria@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/24 13:55:48 by inazaria          #+#    #+#             */
-/*   Updated: 2025/08/02 13:06:53 by inazaria         ###   ########.fr       */
+/*   Updated: 2025/08/04 18:29:08 by inazaria         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,48 +14,42 @@
 # define FT_PING_H
 
 # include <stdio.h> // IWYU pragma: keep
-# include <stdlib.h> // IWYU pragma: keep
+// # include <stdlib.h> // IWYU pragma: keep
 # include <stdbool.h>
-# include <sys/socket.h>
+// # include <sys/socket.h>
 # include <netinet/in.h>
-# include <getopt.h>
-# include <string.h> // IWYU pragma: keep
-# include "../libft/includes/libft.h" // IWYU pragma: keep
-
 # include "ANSI-colors.h"
-
-# define EXIT_SUCCESS	0
-# define EXIT_NO_RPL	1
-# define EXIT_OTHER		2
-# define EXIT_BAD_ARGS	64
-
-# define FUNC_SUCCESS 1
-# define FUNC_FAILURE 0
-
-# define ARG_CNT_BAD	RED "[FATAL] Bad Argument count\n" WHT
-# define ARG_INVALID	RED "[FATAL] Invalid arguments\n" WHT
-
-# define ARG_MISSING(name)			fprintf(stderr, "%s: missing host operator.\n", (name))
-# define OPT_SHORT_INV(name,opt) 	fprintf(stderr, "%s: invalid option -- '%s'\n", (name), (opt))
-# define OPT_LONG_INV(name,opt)		fprintf(stderr, "%s: unrecognized option -- '%s'\n", (name), (opt))
-# define MORE_INFO_MSG(name)		fprintf(stderr, "Try '%s --help' for more information.\n", (name))
-
-# define VERSION "\
-ft_ping 1.0\n\
-No Copyright (C) 2025 inazaria.\n\
-This is free software: you are free to change and redistribute it.\n\
-There is NO WARRANTY, to the extent permitted by law.\n\n\
-Written by Iyan Nazarian.\n\
-"
+# include "defines.h" // IWYU pragma: keep
 
 struct s_ping {
-	const char	*progname;
-	bool		is_verbose;
-	short		exit_code;
-	char		*host;
+	const char		*progname;
+	bool			is_verbose;
+	short			exit_code;
+	char			*host;
+	int			socket;
+	struct sockaddr_in	ip;
+
 };
 
-bool	parse_cli(int argc, char *argv[], struct s_ping *data);
+struct s_icmp_packet {
+	int8_t		type;
+	int8_t		code;
+	int16_t		checksum;
+	int16_t		identifier;
+	int16_t		seq_number;
+};
+
+void	parse_cli(int argc, char *argv[], struct s_ping *data);
+
+// Utils
 void	print_help(const char *progname);
+void	print_info(struct s_ping *data);
+void	fatal(const char *msg);
+void	fatal_strerror(const char *msg);
+void	log_error(const char *msg, FILE *log);
+
+FILE	*get_logfile(void);
+void	close_logfile(void);
+
 
 #endif
