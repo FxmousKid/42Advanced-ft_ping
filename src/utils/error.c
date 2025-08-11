3e08fb14ff2902cdd2df13731b85ab49ed54c433
Iyan Nazarian <36068362+FxmousKid@users.noreply.github.com>
2025-08-11 23:49:03 +0300

    chore: refactoring utils for error mangement

--- src/utils/error.c
+++ src/utils/error.c
@@ -3,52 +3,22 @@
 #include <errno.h>
 #include <string.h>
 
+_Noreturn
+void	fatal(void)
+{
+	exit(EXIT_OTHER);
+}
+
 _Noreturn 
-void	fatal(const char *msg)
+void	fatal_error(const char *msg)
 {
 	fprintf(stderr, "%s[FATAL] %s%s\n", RED, msg, WHT);
-	exit(EXIT_OTHER);
-}
-
-void	fatal_strerror(const char *msg)
-{
-	fprintf(stderr, "%s[FATAL] %s: %s%s\n", RED, msg, strerror(errno), WHT);
-	exit(EXIT_OTHER);
-}
-
-
-FILE	*get_logfile(void)
-{
-	static FILE *logfile = NULL;
-
-	if (!logfile)
-		logfile = fopen(LOGFILE_NAME, "w");
-
-	if (!logfile)
-		fatal_strerror("Log file creation");
-
-	return logfile;
-}
-
-void	log_close(void) 
-{
-	fclose(get_logfile());
-}
-
-void	log_error(const char *msg, FILE *log)
-{
-	fprintf(log, "%s[FATAL] %s%s\n", RED, msg, WHT);
-}
-
-void	log_error_strerror(const char *msg, FILE *log)
-{
-	fprintf(stderr, "%s[FATAL] %s: %s%s\n", RED, msg, strerror(errno), WHT);
-	fprintf(log, "%s[FATAL] %s: %s%s\n", RED, msg, strerror(errno), WHT);
+	fatal();
 }
 
 _Noreturn
-void	log_fatal(const char *msg, FILE *log)
+void	fatal_strerror(const char *msg)
 {
-	log_error(msg, log);
-	fatal(msg);
+	fprintf(stderr, "%s[FATAL] %s: %s%s\n", RED, msg, strerror(errno), WHT);
+	fatal();
 }
---
+++ src/utils/logfile.c
@@ -0,0 +1,63 @@
+/**
+ * @file logfile.c
+ * @brief utilities related to logging
+ * @ingroup utils 
+ *
+ * @author Iyan Nazarian
+ * @date 2025-08-11
+ * @version v0.1.0-1-g927c91f
+ *
+ * @details
+ *  All functions related to logging
+ */
+
+#include "ft_ping.h"
+#include <errno.h>
+#include <string.h>
+
+FILE	*get_logfile(void)
+{
+	static FILE *logfile = NULL;
+
+	if (!logfile)
+		logfile = fopen(LOGFILE_NAME, "w");
+
+	if (!logfile) {
+		log_strerror("Log file creation", get_logfile());
+		fatal_strerror("Log file creation");
+	}
+
+	return logfile;
+}
+
+void	log_close(void) 
+{
+	fclose(get_logfile());
+}
+
+void	log_error(const char *msg, FILE *log)
+{
+	fprintf(log, "%s[FATAL] %s%s\n", RED, msg, WHT);
+}
+
+void	log_success(const char *msg, FILE *log)
+{
+	fprintf(log, "%s[SUCES] %s%s\n", GRN, msg, WHT);
+}
+
+void	log_event(const char *msg, FILE *log)
+{
+	fprintf(log, "%s[EVENT] %s%s\n", YEL, msg, WHT);
+}
+
+void	log_strerror(const char *msg, FILE *log)
+{
+	fprintf(log, "%s[FATAL] %s: %s%s\n", RED, msg, strerror(errno), WHT);
+}
+
+_Noreturn
+void	log_fatal(const char *msg, FILE *log)
+{
+	log_error(msg, log);
+	fatal_error(msg);
+}
